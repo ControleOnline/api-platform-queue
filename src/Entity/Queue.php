d9fd2c9932492c5aa5c355a4c7e032404a46252d
Luiz Kim <luiz.kim@controleonline.com>
2025-02-10 12:59:00 -0300

    Update

--- src/Entity/Queue.php
+++ src/Entity/Queue.php
@@ -75,7 +75,7 @@ class Queue
      * })
      * @Groups({"product_category:read","product:read","product_group_product:read","order_product:read","order:read","order_details:read","order:write","queue:read", "queue:write"})   
      */
-    #[ApiFilter(filterClass: SearchFilter::class, properties: ['type' => 'exact'])]
+    #[ApiFilter(filterClass: SearchFilter::class, properties: ['company' => 'exact'])]
 
     private $company;
 
